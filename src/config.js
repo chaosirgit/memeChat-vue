880ad4bd2ed56b2000e7ac08cf2ee5f598f6ba85
chaosir <qq278935403@gmail.com>
2020-09-04 16:59:18 +0800

    chaosir

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const baseConfig = {
-    baseUrl : '/api/v1/',
+    baseUrl : 'http://chat.memestech.com.cn/api/v1/',
     // baseUrl : 'http://memechat/api/v1/',
     // wsServer: 'ws://memechat:2030'
     wsServer: 'ws://chat.memestech.com.cn:2030'
